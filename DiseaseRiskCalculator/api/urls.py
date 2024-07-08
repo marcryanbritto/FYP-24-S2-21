d560ed79446aac0cd43f9c29329d37f0fae0f97f
marcryanbritto <marcryanbritto@gmail.com>
2024-07-08 15:55:10 +0800

    Update urls.py

--- DiseaseRiskCalculator/api/urls.py
+++ DiseaseRiskCalculator/api/urls.py
@@ -9,7 +9,7 @@ from .views import AccountView, CreateAccountView
 urlpatterns = [
     path('', home, name='home'),
     path('account', AccountView.as_view()),
-    path('create-account', CreateAccountView.as_view()),
+    path('create-account', CreateAccountView.as_view(), name="create-account"),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('calculate/', calculate_risk, name='calculate_risk'),
